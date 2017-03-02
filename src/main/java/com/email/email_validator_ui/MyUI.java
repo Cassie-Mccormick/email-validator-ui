9e9c9fc78a0a038a5559b24d67174f5bd76a0d40
Cassie-Mccormick <McCormick@ta71b.wpa.dal.ca>
2017-03-02 08:58:52 -0400

    added comments

--- src/main/java/com/email/email_validator_ui/MyUI.java
+++ src/main/java/com/email/email_validator_ui/MyUI.java
@@ -11,43 +11,44 @@ import com.vaadin.ui.Label;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
-
+//theme for styling the page
 @Theme("mytheme")
 public class MyUI extends UI {
 
     @Override
     protected void init(VaadinRequest vaadinRequest) {
-        final VerticalLayout layout = new VerticalLayout();
-        
+        final VerticalLayout layout = new VerticalLayout();//layout
+        //creating the text field with a caption above it
         final TextField email = new TextField();
         email.setCaption("Type your email here:");
-
+        //creating a button with verification
         Button button = new Button("Verify Email");
         button.addClickListener( e -> {
-            
+            //getting the verification from the other file
         	email_validator validator = new email_validator();
             boolean validation = validator.email_validator(email.getValue());
             String result = "";
-            
+            //if the email is right
             if (validation == true) {
             	result = "Email is valid.";
             }
+            //if the email is wrong
             else {
             	result = "Email is invalid. Please try again.";
             }
-            
+            //layout of the result
             layout.addComponent(new Label(result));
         });
-        
+        //layout of the page
         layout.addComponents(email, button);
         layout.setMargin(true);
         layout.setSpacing(true);
         
         setContent(layout);
-    }
+    }//end of protected void
 
     @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
     @VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
     public static class MyUIServlet extends VaadinServlet {
     }
-}
+}//end of class
--- src/main/java/com/email/email_validator_ui/email_validator.java
+++ src/main/java/com/email/email_validator_ui/email_validator.java
@@ -2,9 +2,11 @@ package com.email.email_validator_ui;
 
 public class email_validator {
 	public boolean email_validator(String email) {
+		//setting to true
 		boolean validation = true;
 		int count = 0;
 		for (int i = 0; i < email.length(); i++) {
+			//to make sure the email has a @ sign
 			if (email.charAt(i) == '@'); {
 				count++;
 			}
@@ -12,15 +14,18 @@ public class email_validator {
 		if (count != 1) {
 			validation = false;
 		}
+		//to check for a . in the email
 		if (!email.contains(".")) {
 			validation = false;
 		}
+		//if the length is less than 10
 		if (email.length() < 10) {
 			validation = false;
 		}
+		//if the email does not contain the domain names
 		if (!email.contains("gmail") || !email.contains("yahoo") || !email.contains("dal") || !email.contains("hotmail")) {
 			validation = false;
 		}
 		return validation;
-	}
-}
+	}//end of boolean
+}//end of class
