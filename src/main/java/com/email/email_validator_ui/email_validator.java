e2862d655d79d64bb0fc9536cdc8d43178e197e8
Cassie-Mccormick <McCormick@ta71b.wpa.dal.ca>
2017-03-02 09:54:47 -0400

    Changed code due to the original code not working with the UI

--- src/main/java/com/email/email_validator_ui/email_validator.java
+++ src/main/java/com/email/email_validator_ui/email_validator.java
@@ -2,30 +2,13 @@ package com.email.email_validator_ui;
 
 public class email_validator {
 	public boolean email_validator(String email) {
-		//setting to true
-		boolean validation = true;
-		int count = 0;
-		for (int i = 0; i < email.length(); i++) {
-			//to make sure the email has a @ sign
-			if (email.charAt(i) == '@'); {
-				count++;
-			}
+		//if the email matches any of these criteria, it will return true
+		if (email.matches("[^@]*@[^@]*") && email.matches(".*[.].*") && email.matches(".*[.]+((com)|(ca))") && email.matches("^[A-Za-z0-9+_.-]+@(.+)$")) {
+			return true;
 		}
-		if (count != 1) {
-			validation = false;
+		//false if not true
+		else {
+			return false;
 		}
-		//to check for a . in the email
-		if (!email.contains(".")) {
-			validation = false;
-		}
-		//if the length is less than 10
-		if (email.length() < 10) {
-			validation = false;
-		}
-		//if the email does not contain the domain names
-		if (!email.contains("gmail") || !email.contains("yahoo") || !email.contains("dal") || !email.contains("hotmail")) {
-			validation = false;
-		}
-		return validation;
 	}//end of boolean
 }//end of class
